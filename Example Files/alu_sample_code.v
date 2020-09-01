cbd5bf6f7d3a8547917ec918c17fab930ce28a24
Colin Pollard <colionp@dsl5.cs.utah.edu>
2020-09-01 16:29:35 -0600

    Added example verilog files from canvas.

---
+++ Example Files/alu_sample_code.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:54:08 08/30/2011 
+// Design Name: 
+// Module Name:    alu 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module alu( A, B, C, Opcode, Flags
+    );
+input [3:0] A, B;
+input [1:0] Opcode;
+output reg [3:0] C;
+output reg [4:0] Flags;
+
+parameter ADDU = 2'b00;
+parameter ADD = 2'b01;
+parameter SUB = 2'b10;
+parameter CMP = 2'b11;
+
+always @(A, B, Opcode)
+begin
+	case (Opcode)
+	ADDU:
+		begin
+		{Flags[3], C} = A + B;
+		// perhaps if ({Flags[3], C} == 5'b00000) ....
+		if (C == 4'b0000) Flags[4] = 1'b1; 
+		else Flags[4] = 1'b0;
+		Flags[2:0] = 3'b000;
+		end
+	ADD:
+		begin
+		C = A + B;
+		if (C == 4'b0000) Flags[4] = 1'b1;
+		else Flags[4] = 1'b0;
+		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
+		else Flags[2] = 1'b0;
+		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
+
+		end
+	SUB:
+		begin
+		C = A - B;
+		if (C == 4'b0000) Flags[4] = 1'b1;
+		else Flags[4] = 1'b0;
+		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
+		else Flags[2] = 1'b0;
+		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
+		end
+	CMP:
+		begin
+		if( $signed(A) < $signed(B) ) Flags[1:0] = 2'b11;
+		else Flags[1:0] = 2'b00;
+		C = 4'b0000;
+		Flags[4:2] = 3'b000;
+		// both positive or both negative
+		/*if( A[3] == B[3] )
+		begin
+			if (A < B) Flags[1:0] = 2'b11;
+			else Flags[1:0] = 2'b00;
+		end
+		else if (A[3] == 1'b0) Flags[1:0] = 2'b00;
+		else Flags[1:0] = 2'b01;
+		Flags[4:2] = 3'b000;
+		
+		// C = ?? if I don;t specify, then I'm in trouble.
+		C = 4'b0000;
+		*/
+		end
+	default: 
+		begin
+			C = 4'b0000;
+			Flags = 5'b00000;
+		end
+	endcase
+end
+
+endmodule
---
+++ Example Files/alutest_sample_code.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:25:01 08/30/2011
+// Design Name:   alu
+// Module Name:   C:/Documents and Settings/Administrator/ALU/alutest.v
+// Project Name:  ALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: alu
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module alutest;
+
+	// Inputs
+	reg [3:0] A;
+	reg [3:0] B;
+	reg [1:0] Opcode;
+
+	// Outputs
+	wire [3:0] C;
+	wire [4:0] Flags;
+
+	integer i;
+	// Instantiate the Unit Under Test (UUT)
+	alu uut (
+		.A(A), 
+		.B(B), 
+		.C(C), 
+		.Opcode(Opcode), 
+		.Flags(Flags)
+	);
+
+	initial begin
+//			$monitor("A: %0d, B: %0d, C: %0d, Flags[1:0]:
+%b, time:%0d", A, B, C, Flags[1:0], $time );
+//Instead of the $display stmt in the loop, you could use just this
+//monitor statement which is executed everytime there is an event on any
+//signal in the argument list.
+
+		// Initialize Inputs
+		A = 0;
+		B = 0;
+		Opcode = 2'b11;
+
+		// Wait 100 ns for global reset to finish
+/*****
+		// One vector-by-vector case simulation
+		#10;
+	        Opcode = 2'b11;
+		A = 4'b0010; B = 4'b0011;
+		#10
+		A = 4'b1111; B = 4'b 1110;
+		//$display("A: %b, B: %b, C:%b, Flags[1:0]: %b, time:%d", A, B, C, Flags[1:0], $time);
+****/
+		//Random simulation
+		for( i = 0; i< 10; i = i+ 1)
+		begin
+			#10
+			A = $random % 16;
+			B = $random %16;
+			$display("A: %0d, B: %0d, C: %0d, Flags[1:0]: %b, time:%0d", A, B, C, Flags[1:0], $time );
+		end
+		$finish(2);
+		
+		// Add stimulus here
+
+	end
+      
+endmodule
+
