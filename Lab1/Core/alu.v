ee77d99569877cd0c970107cae674ebb44d22da6
Colin Pollard <colin.pollard1466@gmail.com>
2020-09-05 12:05:45 -0600

    Fixed adding op codes, implemented Add operations properly.

--- Lab1/Core/alu.v
+++ Lab1/Core/alu.v
@@ -15,18 +15,32 @@ input [7:0] Opcode;
 output reg [15:0] C;
 output reg [4:0] Flags;
 
+// "The only ones which can change the program status register (PSR) are the arithmetic instructions ADD, ADDI, SUB,SUBI, CMP, CMPI."
+// Flags[0] = Carry bit
+// Flags[1] = Low Flag
+// Flags[2] = Flag Bit
+// Flags[3] = Zero Bit
+// Flags[4] = Negative Bit
+
 parameter ADD    = 8'b00000001;
 parameter ADDI   = 8'b0101xxxx;
+
 parameter ADDU   = 8'b00000110;
 parameter ADDUI  = 8'b0110xxxx;
+
 parameter ADDC   = 8'b00000111;
+parameter ADDCI  = 8'b0111xxxx;
+
 parameter ADDCU  = 8'b00000100;
-parameter ADDCI  = 8'b00001000;
+parameter ADDCUI = 8'b00001000;
+
 parameter SUB    = 8'b00001001;
 parameter SUBI   = 8'b1001xxxx;
+
 parameter CMP    = 8'b00001011;
 parameter CMPI   = 8'b1011xxxx;
 parameter CMPUI  = 8'b00001100;
+
 parameter NOP    = 8'b00000000;
 parameter AND    = 8'b00000001;
 parameter OR     = 8'b00000010;
@@ -38,32 +52,62 @@ parameter RSH    = 8'b01001111;
 parameter RSHI   = 8'b10000101;
 parameter ALSH   = 8'b10000111;
 parameter ARSH   = 8'b10001xxx;
-parameter SUBI   = 8'1001xxxx;
-parameter CMP    = 8'b00001011;
-parameter CMPI   = 8'b1011xxxx;
 
 
 always @(A, B, Opcode)
 begin
 	case (Opcode)
-	ADDU:
-		begin
-		{Flags[3], C} = A + B;
-		// perhaps if ({Flags[3], C} == 5'b00000) ....
-		if (C == 4'b0000) Flags[4] = 1'b1; 
-		else Flags[4] = 1'b0;
-		Flags[2:0] = 3'b000;
-		end
-	ADD:
+	
+	// Add Signed or Signed Immediate, changes flags.
+	ADD,
+	ADDI:
 		begin
 		C = A + B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
+		if (C == 16'b0000000000000000) Flags[3] = 1'b1;
+		else Flags[3] = 1'b0;
+		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
 		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-
+		Flags[1:0] = 2'b00; Flags[4] = 1'b0;
 		end
+		
+	//ADDC (or ADDCI) does the same as ADD (signed) except the C flag is also added in. It affects the same flags
+	ADDC,
+	ADDCI:
+		begin
+		// Add A + B + Carry bit
+		C = A + B + Flags[0];
+		
+		// If zero result set Z bit
+		if (C == 16'b0000000000000000) Flags[3] = 1'b1;
+		else Flags[3] = 1'b0;
+		
+		// If signed overflow occured set Flag bit
+		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
+		else Flags[2] = 1'b0;
+		
+		// All other flags 0.
+		Flags[1:0] = 2'b00; Flags[4] = 1'b0;
+		end
+	
+	// Add Unsigned or Unsigned Immediate
+	// According to the ISA, these do the same thing as ADD but do not affect the PSR.
+	ADDU,
+	ADDUI:
+		begin
+		C = A + B;
+		Flags = 5'b00000;
+		end
+	
+	//ADDCU (or ADDCUI) does the same as ADDU (unsigned) except the C flag is also added in. No changes to flags.
+	ADDCU,
+	ADDCUI:	
+		begin
+		// Set the carry bit if result is 17 bits
+		{Flags[0], C} = A + B + Flags[0];
+		
+		Flags = 5'b00000;
+		end
+		
 	SUB:
 		begin
 		C = A - B;
@@ -92,71 +136,6 @@ begin
 		// C = ?? if I don;t specify, then I'm in trouble.
 		C = 4'b0000;
 		*/
-		end
-		
-	ADDI:
-		begin
-		{Flags[3], C} = A + B;
-		// perhaps if ({Flags[3], C} == 5'b00000) ....
-		if (C == 4'b0000) Flags[4] = 1'b1; 
-		else Flags[4] = 1'b0;
-		Flags[2:0] = 3'b000;
-		end
-	ADDUI:
-		begin
-		C = A + B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
-		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-
-		end
-	ADDC:
-		begin
-		C = A - B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
-		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-		end
-	ADDCU:
-		begin
-		if( $signed(A) < $signed(B) ) Flags[1:0] = 2'b11;
-		else Flags[1:0] = 2'b00;
-		C = 4'b0000;
-		Flags[4:2] = 3'b000;
-		// both positive or both negative
-		/*if( A[3] == B[3] )
-		begin
-			if (A < B) Flags[1:0] = 2'b11;
-			else Flags[1:0] = 2'b00;
-		end
-		else if (A[3] == 1'b0) Flags[1:0] = 2'b00;
-		else Flags[1:0] = 2'b01;
-		Flags[4:2] = 3'b000;
-		
-		// C = ?? if I don;t specify, then I'm in trouble.
-		C = 4'b0000;
-		*/
-		end
-			ADDU:
-		begin
-		{Flags[3], C} = A + B;
-		// perhaps if ({Flags[3], C} == 5'b00000) ....
-		if (C == 4'b0000) Flags[4] = 1'b1; 
-		else Flags[4] = 1'b0;
-		Flags[2:0] = 3'b000;
-		end
-	ADDCI:
-		begin
-		C = A + B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
-		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
 		end
 	SUBI:
 		begin
@@ -201,17 +180,25 @@ begin
 		Flags = 5'b0000;
 		C = !A;
 		end
+	LSH:
+		begin
+		Flags = 5'b00000;
+		C = A << 1;
+		end
 	LSHI:
 		begin
-		
+		Flags = 5'b00000;
+		C = A << B;
 		end
 	RSH:
 		begin
-		
+		Flags = 5'b00000;
+		C = A >> 1;
 		end
 	RSHI:
 		begin
-		
+		Flags = 5'b00000;
+		C = A >> B;
 		end
 	default: 
 		begin
