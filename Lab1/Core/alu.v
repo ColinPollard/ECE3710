7ea367e7e6fe4f91e9191cdedd9f99fabea76ee4
Colin Pollard <colin.pollard1466@gmail.com>
2020-09-05 12:59:01 -0600

    Finished first implementation of all operations. Added arithmetic shift.

--- Lab1/Core/alu.v
+++ Lab1/Core/alu.v
@@ -1,15 +1,20 @@
 /*
- * Lab 1 ALU design
- *	Group 4
- *
- *
+ * Authors:
+	Colin Pollard, Ian Lavin, Luke Majors, Mckay Mower
+ * 
+ * Date Created:
+	9/3/2020
+	
+ * 16-bit EECS 427 ALU Design
+	This project represents a 16-bit risc ALU that implements the baseline standard for
+	the EECS 427. It utilizes an internal 5-bit flags register, and assumes that immediate
+	sign extentions are handled outside of the ALU.
  */
 
 
 `timescale 1ns / 1ps
 
-module alu( A, B, C, Opcode, Flags
-    );
+module alu( A, B, C, Opcode, Flags);
 input [15:0] A, B;
 input [7:0] Opcode;
 output reg [15:0] C;
@@ -46,6 +51,7 @@ parameter AND    = 8'b00000001;
 parameter OR     = 8'b00000010;
 parameter XOR    = 8'b00000011;
 parameter NOT    = 8'b00001111;
+
 parameter LSH    = 8'b00000111;
 parameter LSHI   = 8'b1000000x;
 parameter RSH    = 8'b01001111;
@@ -57,6 +63,8 @@ parameter ARSH   = 8'b10001xxx;
 always @(A, B, Opcode)
 begin
 	case (Opcode)
+	
+	// Adding operations -----------------------------------------------------------------------------------------------------------------------------
 	
 	// Add Signed or Signed Immediate, changes flags.
 	ADD,
@@ -103,58 +111,77 @@ begin
 	ADDCUI:	
 		begin
 		// Set the carry bit if result is 17 bits
-		{Flags[0], C} = A + B + Flags[0];
+		C = A + B + Flags[0];
 		
+		// Set all flags to 0
 		Flags = 5'b00000;
 		end
 		
-	SUB:
-		begin
-		C = A - B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
-		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
-		end
-	CMP:
-		begin
-		if( $signed(A) < $signed(B) ) Flags[1:0] = 2'b11;
-		else Flags[1:0] = 2'b00;
-		C = 4'b0000;
-		Flags[4:2] = 3'b000;
-		// both positive or both negative
-		/*if( A[3] == B[3] )
-		begin
-			if (A < B) Flags[1:0] = 2'b11;
-			else Flags[1:0] = 2'b00;
-		end
-		else if (A[3] == 1'b0) Flags[1:0] = 2'b00;
-		else Flags[1:0] = 2'b01;
-		Flags[4:2] = 3'b000;
 		
-		// C = ?? if I don;t specify, then I'm in trouble.
-		C = 4'b0000;
-		*/
-		end
+	// Subtracting operations ------------------------------------------------------------------------------------------------------------------------
+	
+	// Subtract or Subtract immediate affects PSR.
+	SUB,
 	SUBI:
 		begin
 		C = A - B;
-		if (C == 4'b0000) Flags[4] = 1'b1;
-		else Flags[4] = 1'b0;
-		if( (~A[3] & ~B[3] & C[3]) | (A[3] & B[3] & ~C[3]) ) Flags[2] = 1'b1;
+		
+		// If result is zero set zero flag.
+		if (C == 16'b0000000000000000) Flags[3] = 1'b1;
+		else Flags[3] = 1'b0;
+		
+		// If signed subtraction results in overflow set flag bit.
+		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
 		else Flags[2] = 1'b0;
-		Flags[1:0] = 2'b00; Flags[3] = 1'b0;
+		
+		// All other flags zero.
+		Flags[1:0] = 2'b00; Flags[4] = 1'b0;
 		end
 		
+	// "CMP and CMPI perform the same operations as SUB, SUBI but affect different PSR flags (see below) and do not write back the result."
+	// "Set the Z flag if the result is zero, set the L flag if Rsrc/Imm > Rdest when the operands are treated as unsigned numbers."
+	CMP,
 	CMPI:
 		begin
+		// Set the low flag if A < B signed
+		if( $signed(A) < $signed(B) ) Flags[1] = 1'b1;
+		else Flags[1] = 1'b0;
 		
+		// Set zero flag if result is zero.
+		if (A == B) Flags[3] = 1'b1;
+		else Flags[3] = 1'b0;
+		
+		// There is no write back, so we will just set C to 0
+		C = 4'b0000;
+		
+		// All other flags zero.
+		Flags[0] = 1'b0;
+		Flags[2] = 1'b0;
+		Flags[4] = 1'b0;
 		end
+	
+	
+	// "Set the Z flag if the result is zero, set the N flag if Rsrc/Imm > Rdest when the operands are treated as two’s complement numbers"
+	CMPU,
 	CMPUI:
 		begin
+		// Set the N flag if A < B signed
+		if(A < B) Flags[4] = 1'b1;
+		else Flags[4] = 1'b0;
 		
+		// Set zero flag if result is zero.
+		if (A == B) Flags[3] = 1'b1;
+		else Flags[3] = 1'b0;
+		
+		// There is no write back, so we will just set C to 0
+		C = 4'b0000;
+		
+		// All other flags zero.
+		Flags[2:0] = 3'b000;
 		end
+		
+	// Bitwise Operations ----------------------------------------------------------------------------------------------------------------------------
+	
 	NOP:
 		begin
 		Flags = 5'b0000;
@@ -180,29 +207,56 @@ begin
 		Flags = 5'b0000;
 		C = !A;
 		end
+	
+	// Shifting Operations ---------------------------------------------------------------------------------------------------------------------------	
+	
+	// Shift left by 1, not specified in ISA but is specified in Lab1-20.pdf
 	LSH:
 		begin
 		Flags = 5'b00000;
 		C = A << 1;
 		end
+	
+	// Shift left by immediate. Range is -15 to 15 (5 bits)
 	LSHI:
 		begin
 		Flags = 5'b00000;
-		C = A << B;
+		C = A << $signed(B[4:0]);
 		end
+		
+	// Right shift by 1
 	RSH:
 		begin
 		Flags = 5'b00000;
 		C = A >> 1;
 		end
+	
+	// Right shift by immediate. Range is -15 to 15 (5 bits)
 	RSHI:
 		begin
 		Flags = 5'b00000;
-		C = A >> B;
+		C = A >> $signed(B[4:0]);
 		end
+		
+	// Arithmetic left shift
+	ALSH:
+		begin
+		Flags = 5'b00000;
+		C = A <<< $signed(B[4:0]);
+		end
+	
+	// Arithmetic right shift
+	ARSH:
+		begin
+		Flags = 5'b00000;
+		C = A >>> $signed(B[4:0]);
+		end
+		
+	// Default case ----------------------------------------------------------------------------------------------------------------------------------
+		
 	default: 
 		begin
-			C = 4'b0000;
+			C = 16'b0000000000000000;
 			Flags = 5'b00000;
 		end
 	endcase
