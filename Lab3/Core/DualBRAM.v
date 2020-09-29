7ac55d494dc16ac20a04e4ea9c32f96ef671a51b
Colin Pollard <colin.pollard1466@gmail.com>
2020-09-29 16:36:39 -0600

    Modified ram module.

--- Lab3/Core/DualBRAM.v
+++ Lab3/Core/DualBRAM.v
@@ -1,9 +1,45 @@
-module DualBRAM(AddressA, DataInA, DataOutA, clkA, enableA, writeEnableA, AddressB, DataInB, DataOutB, clkB, enableB, writeEnableB);
-input [9:0] AddressA, AddressB;
-input [15:0] DataInA, DataInB;
-input clkA, clkB, enableA, enableB, writeEnableA, writeEnableB;
+// Author: Colin Pollard, Ian Lavin, Luke Majors, McKay Mower
+// Date: 9/29/2020
+// This module represents a true dual port dual clock RAM with a data width of 16 and an address width of 10.
 
-output [15:0] DataOutA, DataOutB;
+module DualBRAM
+#(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=10)
+(
+	input [(DATA_WIDTH-1):0] data_a, data_b,
+	input [(ADDR_WIDTH-1):0] addr_a, addr_b,
+	input we_a, we_b, clk_a, clk_b,
+	output reg [(DATA_WIDTH-1):0] q_a, q_b
+);
 
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+
+	always @ (posedge clk_a)
+	begin
+		// Port A 
+		if (we_a) 
+		begin
+			ram[addr_a] <= data_a;
+			q_a <= data_a;
+		end
+		else 
+		begin
+			q_a <= ram[addr_a];
+		end 
+	end
+
+	always @ (posedge clk_b)
+	begin
+		// Port B 
+		if (we_b) 
+		begin
+			ram[addr_b] <= data_b;
+			q_b <= data_b;
+		end
+		else 
+		begin
+			q_b <= ram[addr_b];
+		end 
+	end
 
 endmodule
