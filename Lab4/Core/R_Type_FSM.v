17c52237f66f55db2fa2a5199ee2e07299fdf7c8
McKay Mower <mmower777@gmail.com>
2020-10-18 16:49:17 -0600

    Started FSM for R type instructions

--- Lab4/Core/R_Type_FSM.v
+++ Lab4/Core/R_Type_FSM.v
@@ -2,9 +2,12 @@
 // Date: 10/15/2020
 // This file creates an FSM to control basic R-type instructions.
 
-module R_Type_FSM(clk, rst, PC_enable);
+module R_Type_FSM(clk, rst, PC_enable, R_enable, R_or_I);
 	input clk, rst;
 	
+	output reg PC_enable;
+	output reg R_enable;
+	output reg R_or_I; //0 for R, 1 for I
 	
 	// FSM States --------------------------------------------------------------------------------
 	
@@ -12,53 +15,41 @@ module R_Type_FSM(clk, rst, PC_enable);
 	reg[3:0] y;
 	
 	//Parameters for the fsm states
-	parameter[3:0] S0 = 4'h00, S1 = 4'h01, S2 = 4'h02, S3 = 4'h03, S4 = 4'h04, S5 = 4'h05;
+	parameter[3:0] S0 = 4'h00, S1 = 4'h01, S2 = 4'h02;
 						
 	//Update state
 	always @(posedge clk)
 	begin
 		if(rst) y <= S0;
-		else if(y == S5)
-			y <= y;
+		else if(y == S2)
+			y <= S0;
 		else begin
 			y <= y + 1'b1;
 		end
 	end
-	
 	
 	//Update output
 	// Expected output is: 1, 2, 3, 4, 5, 0.
 	always @(y)
 	begin
 		case(y)
-			S0: begin // Read from address 0 (expect 1) on A
-					
-			
+			S0: begin 
+			PC_enable = 1'b0;
+			R_en = 1'b0;
+			R_or_I = 1'bx;
 			end
-			S1: begin // Read from address 1 (expect 2) on B
-			
+			S1: begin
+			PC_enable = 1'b0;
+			R_en = 1'b0;
+			R_or_I = 1'bx;
 			end
-			S2: begin // Write to address 0 on A, read from address 2 on B
-			
-			
-			end
-			S3: begin // Read from address 0 on A, write to address 513 on B
-			
-			
-
-			end
-			S4: begin // Read from address 513 on A, reset value at address 0 to 1 on B.
-			
-
-			end
-			S5: begin // Reset value at address 513 to 0 on A. Read from 5 on B.
-			
-			
-
+			S2: begin
+			PC_enable = 1'b1;
+			R_en = 1'b0; //
+			R_or_I = 1'bx; //
 			end
 			
 			default: begin // Do nothing
-				
 			end
 		endcase
 	end
